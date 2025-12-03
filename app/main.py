4829472c77912358f0e0ac63326ecbca9481e587
maryem <maryemelbergui@gmail.com>
2025-12-03 14:21:39 +0100

    SC-5 Endpoint /signup

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
-from database import Base
+from app.database import Base,engine
 from fastapi import FastAPI
-from database import engine
 from sqlalchemy_utils import database_exists, create_database
-
+from .routes.register import router as signup_router
 app=FastAPI()
 
 if not database_exists(engine.url):
     create_database(engine.url)
 Base.metadata.create_all(bind=engine)
 
-@app.get('/')
-def initialize():
-    return "app loaded!"
+
+app.include_router(signup_router)
